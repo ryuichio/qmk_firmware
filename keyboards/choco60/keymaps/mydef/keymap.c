81ce2f2c0b8609f9ffb1e4572498e511ca603662
ryuichio <>
2019-08-10 18:58:42 +0900

    [Choco60] modified for my keymap

--- keyboards/choco60/keymaps/mydef/keymap.c
+++ keyboards/choco60/keymaps/mydef/keymap.c
@@ -27,19 +27,28 @@ enum layer_names {
 
 #define KC_FN MO(_FN)
 
+//#define MAC
+#ifdef MAC
+#define SP_IME_ON   KC_LANG1
+#define SP_IME_OFF  KC_LANG2
+#else
+#define SP_IME_ON   KC_HENK
+#define SP_IME_OFF  KC_MHEN
+#endif
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT(
-    KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINUS, KC_EQUAL, KC_BSLASH, KC_GRAVE,
-    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRACKET, KC_RBRACKET, KC_BSPACE,
-    KC_LCTRL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCOLON, KC_QUOTE, KC_ENTER,
-    KC_LSHIFT,KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLASH, KC_RSHIFT, KC_FN,
-    KC_LALT,  KC_LGUI, KC_SPACE, KC_SPACE, KC_SPACE, KC_RGUI, KC_RALT
+    KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    /**/ KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINUS,    KC_EQUAL,    KC_GRAVE,  KC_BSPC,
+    KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    /**/ KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRACKET, KC_RBRACKET, KC_BSLASH,
+    KC_LCTRL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    /**/ KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOTE,    KC_ENTER,
+    KC_LSHIFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    /**/ KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSHIFT,   KC_FN,
+               KC_LGUI, KC_LALT, KC_SPACE,                  /**/ KC_FN,   KC_BSPC, SP_IME_OFF, SP_IME_ON
   ),
   [_FN] = LAYOUT(
-    _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_INSERT, KC_DELETE,
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCREEN, KC_SCROLLLOCK, KC_PAUSE, KC_UP, KC_RBRACKET, KC_BSPACE,
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_LEFT, KC_RIGHT, KC_ENTER,
-    RESET, _______, _______, _______, _______, _______, _______, _______, KC_END, KC_PGDOWN, KC_DOWN, KC_RSHIFT, KC_FN,
-    _______, _______, _______, _______, _______, KC_STOP, _______
+    _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   /**/ KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,    KC_F12,  KC_INSERT, KC_DEL,
+    _______,   KC_PSCR, KC_SLCK, KC_PAUS, _______, _______, /**/ _______, KC_PGUP, KC_UP,   KC_PGDN, _______, _______,   _______, _______,
+    _______,   _______, _______, _______, _______, _______, /**/ KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, KC_END,  _______,   KC_ENTER,
+    RESET,     _______, _______, _______, _______, _______, /**/ _______, _______, _______, _______, _______, KC_RSHIFT, KC_FN,
+               _______, _______, _______,                   /**/ _______, KC_DEL,  _______, _______
   )
 };
