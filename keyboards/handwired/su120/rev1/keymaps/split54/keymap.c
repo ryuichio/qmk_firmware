68d22234b52eb668498e31534cdd6ca0b728000c
ryuichio <>
2019-10-22 11:25:03 +0900

    [SU120/SPLIT54] modified lower/raise layer keymaps

--- keyboards/handwired/su120/rev1/keymaps/split54/keymap.c
+++ keyboards/handwired/su120/rev1/keymaps/split54/keymap.c
@@ -25,60 +25,64 @@ enum custom_keycodes {
   SEND_000
 };
 
+// keymap layer number
+enum keymap_layer {
+  KL_BASE,
+  KL_LOWER,
+  KL_RAISE,
+};
+#define MO_LOWER    MO(KL_LOWER)
+#define MO_RAISE    MO(KL_RAISE)
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  /* Default Layer
-   *  Master Side (USB Cable Connected Side)
-   *  ,---------------------------------------------------
-   *  |    =    (    )   BS  Esc   F1   F2   F3   F4   F5
-   *  | NmLk  KP/  KP*  Tab Hank    1    2    3    4    5
-   *  |  KP7  KP8  KP9  KP-  Tab    Q    W    E    R    T
-   *  |  KP4  KP5  KP6  KP+ Caps    A    S    D    F    G
-   *  |  KP1  KP2  KP3 Entr Shft    Z    W    C    V    B
-   *  |  KP0   00    .    , Ctrl  GUI  Alt MHEN Spce Spce
-   *  `---------------------------------------------------
-   *  Slave Side (USB Cable Not Connected Side)
-   *  ,---------------------------------------------------
-   *  |   F6   F7   F8   F9  F10  F11  F12 PtSc  Del  Ins
-   *  |    6    7    8    9    0    -    =   BS   BS Home
-   *  |    Y    U    I    O    P    [    ]    \ Entr PgUp
-   *  |    H    J    K    L    ;    ' Entr Entr Entr PgDn
-   *  |    N    M    ,    .    / Shft Shft Shft   Up  End
-   *  | Spce  HEN KANA  Alt  App Ctrl MO(1)Left Down Rght
-   *  `---------------------------------------------------
-   */
+  [KL_BASE] = LAYOUT( /* Base */
+/* R1 */    KC_ESC,   KC_GRAVE, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* R3 */    KC_LCTRL, KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* R4 */    KC_LSHIFT,KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* R5 */    KC_LCTRL, KC_LGUI,  KC_LALT,  MO_LOWER, XXXXXXX,  KC_SPACE, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* R2 */    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* NA */    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
 
-  [0] = LAYOUT( /* Base */
-    KC_ESC,   KC_GRAVE, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    KC_LCTRL, KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    KC_LSHIFT,KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    KC_LCTRL, KC_LGUI,  KC_LALT,  MO(1),    XXXXXXX,  KC_SPACE, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_MINUS, KC_EQUAL, KC_BSLASH,XXXXXXX,  XXXXXXX,  XXXXXXX,
-    KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,   XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    KC_BSPC,  XXXXXXX,  MO(1),    KC_RALT,  KC_LEFT,  KC_DOWN,  KC_RGHT,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_UP,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX
+/* R1 */    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC_MINUS, KC_EQUAL, KC_BSLASH,XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* R2 */    KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* R3 */    KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,  KC_ENT,   XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* NA */    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* R5 */    KC_BSPC,  XXXXXXX,  MO_RAISE, KC_RALT,  KC_LEFT,  KC_DOWN,  KC_RGHT,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* R4 */    KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_UP,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX
   ),
 
-  [1] = LAYOUT(
-    RGB_TOG,  RGB_MOD,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    RGB_HUI,  RGB_SAI,  RGB_VAI,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    RGB_HUD,  RGB_SAD,  RGB_VAD,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  RESET,
+  [KL_LOWER] = LAYOUT(
+/* R1 */    _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* R3 */    _______,  KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* R4 */    _______,  _______,  _______,  _______,  _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* R5 */    _______,  _______,  _______,  _______,  XXXXXXX,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* R2 */    _______,  KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* NA */    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
 
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX
+/* R1 */    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* R2 */    KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* R3 */    KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_LPRN,  KC_RPRN,  _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* NA */    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* R5 */    _______,  XXXXXXX,  _______,  _______,  KC_HOME,  KC_PGDN,  KC_END,   XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* R4 */    _______,  _______,  _______,  _______,  _______,  KC_PGUP,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX
   ),
+
+  [KL_RAISE] = LAYOUT(
+/* R1 */    _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* R3 */    _______,  _______,  _______,  _______,  _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* R4 */    _______,  _______,  _______,  _______,  _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* R5 */    _______,  _______,  _______,  _______,  XXXXXXX,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* R2 */    _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* NA */    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+
+/* R1 */    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* R2 */    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* R3 */    _______,  _______,  _______,  _______,  _______,  _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* NA */    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* R5 */    KC_DEL,   XXXXXXX,  _______,  _______,  _______,  _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+/* R4 */    _______,  _______,  _______,  _______,  _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX
+  )
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
