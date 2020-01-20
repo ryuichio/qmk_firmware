647976b9a9015e9082a1faf241b6d92592967d03
ryuichio <>
2020-01-21 08:01:32 +0900

    [slamd] add ime functionalities for layer keys

--- keyboards/slamd/keymaps/default/keymap.c
+++ keyboards/slamd/keymaps/default/keymap.c
@@ -20,10 +20,13 @@ enum slamd_layouts {
   _BASE,
   _LOWER,
   _RAISE,
+  _ADJUST,
 };
 
-enum planck_keycodes {
-  BASE = SAFE_RANGE,
+enum custom_keycodes {
+  LOWER = SAFE_RANGE,
+  RAISE,
+  ADJUST,
 };
 
 enum tapdances{
@@ -36,10 +39,7 @@ enum tapdances{
 #define KC_SLBS  TD(TD_SLBS)
 #define KC_MIEQ  TD(TD_MIEQ)
 
-#define LOWER MO(_LOWER)
-#define RAISE MO(_RAISE)
-
-#define _MAC
+//#define _MAC
 #ifdef _MAC
 #define SP_CTRL  KC_LGUI
 #define SP_WIN   KC_LCTL
@@ -82,20 +82,117 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Raise */
 [_RAISE] = LAYOUT(
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, KC_PGUP, KC_UP,   KC_PGDN, _______, _______,
-    _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT,_______, _______,
+    _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT,KC_END,  _______,
     _______, KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, KC_DEL,  _______, _______, _______, _______, _______
+),
+/* Adjust */
+[_ADJUST] = LAYOUT(
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 )
 };
 
-void matrix_init_user(void) {
+static inline void _send_key(uint16_t keycode) {
+    register_code(keycode);
+    unregister_code(keycode);
+}
 
+static inline void _change_ime(bool enable) {
+    if (enable) {
+#ifdef _MAC
+        _send_key(KC_LANG1);
+#else
+        _send_key(KC_HENK);
+#endif
+    } else {
+#ifdef _MAC
+        _send_key(KC_LANG2);
+#else
+        _send_key(KC_MHEN);
+#endif
+    }
+}
+
+static bool lower_pressed = false;
+static bool raise_pressed = false;
+static uint16_t lower_pressed_time = 0;
+static uint16_t raise_pressed_time = 0;
+
+static inline void update_change_layer(bool pressed, uint8_t layer1, uint8_t layer2, uint8_t layer3) {
+  pressed ? layer_on(layer1) : layer_off(layer1);
+  IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2) ? layer_on(layer3) : layer_off(layer3);
+}
+
+static inline void set_lower_pressed(uint16_t time) {
+  lower_pressed = true;
+  raise_pressed = false;
+  lower_pressed_time = time;
+}
+
+static inline void set_raise_pressed(uint16_t time) {
+  raise_pressed = true;
+  lower_pressed = false;
+  raise_pressed_time = time;
+}
+
+static inline void reset_layer_pressed(void) {
+  lower_pressed = false;
+  raise_pressed = false;
+  lower_pressed_time = 0;
+  raise_pressed_time = 0;
+}
+
+static inline bool is_lower_pressed(uint16_t time) {
+  return lower_pressed && (TIMER_DIFF_16(time, lower_pressed_time) < TAPPING_TERM);
+}
+
+static inline bool is_raise_pressed(uint16_t time) {
+  return raise_pressed && (TIMER_DIFF_16(time, raise_pressed_time) < TAPPING_TERM);
+}
+
+void matrix_init_user(void) {
 }
 
 void matrix_scan_user(void) {
-
 }
 
 void led_set_user(uint8_t usb_led) {
+}
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case LOWER:
+      if (record->event.pressed) {
+        set_lower_pressed(record->event.time);
+      } else {
+        if (is_lower_pressed(record->event.time)) {
+            _change_ime(false);
+        }
+        lower_pressed = false;
+      }
+      update_change_layer(record->event.pressed, _LOWER, _RAISE, _ADJUST);
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        set_raise_pressed(record->event.time);
+      } else {
+        if (is_raise_pressed(record->event.time)) {
+            _change_ime(true);
+        }
+        raise_pressed = false;
+      }
+      update_change_layer(record->event.pressed, _RAISE, _LOWER, _ADJUST);
+      return false;
+      break;
+    default:
+      if (record->event.pressed) {
+        reset_layer_pressed();
+      }
+      break;
+  }
+  return true;
 }
