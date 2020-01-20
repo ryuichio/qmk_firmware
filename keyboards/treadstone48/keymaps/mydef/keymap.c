e124537029f19c5be1804c67ee4970c2d86911e6
ryuichio <>
2020-01-21 06:52:07 +0900

    [TS48] changed keymap

--- keyboards/treadstone48/keymaps/mydef/keymap.c
+++ keyboards/treadstone48/keymaps/mydef/keymap.c
@@ -74,11 +74,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,--------------------------------------------------------------------------------------------------------------------.
        KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,           KC_EQL,\
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+-----------------|
-      KC_TBSF,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,            _____,\
+      KC_TBSF, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,            _____,\
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------+--------|
-        _____,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,        \
+        _____,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX, KC_LBRC, KC_RBRC, KC_PGUP,        \
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------+--------|
-        _____,   _____,   _____,   _____,            _____,            _____,   _____,   XXXXX,   XXXXX,   XXXXX,  XXXXX,\
+        _____,   _____,   _____,   _____,            _____,            _____,   _____,   XXXXX, KC_HOME, KC_PGDN,  KC_END,\
   //`-------------------------------------------------------------------------------------------------------------------'
      KC_MINS \
   // ExtraKey: Split backspace key or it is below the enter key.
@@ -86,21 +86,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RAISE] = LAYOUT_base( \
   //,--------------------------------------------------------------------------------------------------------------------.
-        _____,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,           KC_F12,\
+        _____,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   XXXXX, KC_PGUP,   KC_UP, KC_PGDN,   XXXXX,           XXXXX,\
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+-----------------|
-      KC_TBSF,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX, KC_LBRC, KC_RBRC,            _____,\
+      KC_TBSF,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT,  KC_END,            _____,\
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------+--------|
-        _____,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX, KC_PGUP,         \
+        _____,  KC_F11,  KC_F12,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX,   XXXXX, KC_PGUP,         \
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------+--------|
         _____,   _____,   _____,   _____,            _____,           KC_DEL,   _____,   XXXXX, KC_HOME, KC_PGDN,  KC_END,\
   //`-------------------------------------------------------------------------------------------------------------------'
-      KC_F11 \
+      XXXXX \
   // ExtraKey: Split backspace key or it is below the enter key.
   ),
 
   [_ADJUST] = LAYOUT_base( \
   //,--------------------------------------------------------------------------------------------------------------------.
-        XXXXX,   RESET,  RGBRST, AG_NORM, AG_SWAP,   XXXXX,   XXXXX, KC_WH_L, KC_WH_U, KC_HOME, KC_PGUP,            XXXXX,\
+        RESET,  RGBRST,   XXXXX, AG_NORM, AG_SWAP,   XXXXX,   XXXXX, KC_WH_L, KC_WH_U, KC_HOME, KC_PGUP,            XXXXX,\
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+-----------------|
         XXXXX, RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI,   XXXXX,   XXXXX, KC_WH_R, KC_WH_D,  KC_END, KC_PGDN,            XXXXX,\
   //|--------+--------+--------+--------+--------+--------|--------+--------+--------+--------+--------+--------+--------|
