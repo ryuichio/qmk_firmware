df35c5118fc7370ce88d0d37a2efc9be4ee1821e
ryuichio <>
2020-11-23 16:10:57 +0900

    [planck] fixed lower layer keymap

--- keyboards/planck/keymaps/mydef/keymap.c
+++ keyboards/planck/keymaps/mydef/keymap.c
@@ -76,11 +76,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Lower */
 [_LOWER] = LAYOUT_planck_grid(
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   XXXXXXX, KC_PGUP, KC_UP,   KC_PGDN, XXXXXXX, XXXXXXX,
-    _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_HOME, KC_LEFT, KC_DOWN, KC_RIGHT,KC_END,  _______,
-    _______, KC_F11,  KC_F12,  XXXXXXX, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    _______, _______, _______, _______, _______, KC_MLAD, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
-),
+    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL,
+    KC_TAB,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_QUOT,
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR, KC_LBRC, KC_RBRC, KC_RCBR, KC_LABK, KC_RABK, XXXXXXX, XXXXXXX,
+    _______, _______, _______, _______, _______, _______, KC_DEL,  _______, XXXXXXX, XXXXXXX, XXXXXXX, KC_TILD
+    ),
 
 /* Raise */
 [_RAISE] = LAYOUT_planck_grid(
